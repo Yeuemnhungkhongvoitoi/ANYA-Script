29c97f88ec16b2b24dab8bbb6247154858dc65ec
Yeuemnhungkhongvoitoi <nnajpa001@gmail.com>
2026-04-24 17:13:52 +0700

    Add files via upload

---
+++ testanya.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Yeuemnhungkhongvoitoi/ANYA-Script/refs/heads/main/check.txt"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Yeuemnhungkhongvoitoi/ANYA-Script/refs/heads/main/testkick.txt"))()

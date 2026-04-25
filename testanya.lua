c4069a2c9efe9872108dd8c42d277902ef7848c3
Yeuemnhungkhongvoitoi <nnajpa001@gmail.com>
2026-04-25 20:11:16 +0700

    Delete testanya.lua

--- testanya.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Yeuemnhungkhongvoitoi/ANYA-Script/refs/heads/main/check.txt"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Yeuemnhungkhongvoitoi/ANYA-Script/refs/heads/main/testkick.txt"))()
